a16266c7442da7a76c911766bd12d225a0341ea1
shadarf779 <sa1121@cs.soran.edu.iq>
2024-07-07 10:25:30 +0300

    Initial commit

---
+++ A Single Neuron.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib as plt
+
+inputs = [1.0 , 2.0 , 3.0]
+weights = [0.2 , 0.8 , -0.5]
+bias = 2.0
+
+output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + bias
+print(output)
