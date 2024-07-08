d0761c979f4a7592b0004dac075b4d5111459704
shadarf779 <sa1121@cs.soran.edu.iq>
2024-07-08 17:18:57 +0300

    Loss_CategoricalCrossentropy

---
+++ CHAPTER V/The Categorical Cross-Entropy Loss Class/Loss_CategoricalCrossentropy.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+class Loss_CategoricalCrossentropy:
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[range(samples), y_true]
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(y_pred_clipped * y_true, axis=1)
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
+    
+    def calculate(self, y_pred, y_true):
+        sample_losses = self.forward(y_pred, y_true)
+        data_loss = np.mean(sample_losses)
+        return data_loss
+
+# Example usage
+softmax_outputs = np.array([[0.7, 0.1, 0.2], [0.1, 0.5, 0.4], [0.02, 0.9, 0.08]])
+class_targets = np.array([0, 1, 1])  # Class indices for three samples
+
+loss_function = Loss_CategoricalCrossentropy()
+loss = loss_function.calculate(softmax_outputs, class_targets)
+print(loss)
