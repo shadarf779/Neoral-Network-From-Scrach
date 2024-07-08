358a3219322682785b30ec4402c0a7047e72b708
shadarf779 <sa1121@cs.soran.edu.iq>
2024-07-08 14:19:24 +0300

    SOFTMAX

---
+++ CHAPTER IV/SoftMax/Softmax.py
@@ -0,0 +1,47 @@
+import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
+
+# Initialize nnfs
+nnfs.init()
+
+class ActivationSoftmax:
+    def forward(self,inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
+
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+softmax = ActivationSoftmax()
+softmax.forward([[1, 2, 3]])
+
+
+softmax.forward([[-2, -1, 0]])
+
+
+softmax.forward([[0.5, 1, 1.5]])
+
+
+X, y = spiral_data(samples=100, classes=3)
+
+dense1 = Layer_Dense(2, 3)
+activation1 = Activation_ReLU()
+dense2 = Layer_Dense(3, 3)
+activation2 = ActivationSoftmax()
+
+dense1.forward(X)
+activation1.forward(dense1.output)
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+print(activation2.output[:5])
